013a2df1ccb565fde217baf64a509789e63920f9
Даня <diman100l@mail.ru>
2020-08-04 11:43:16 +0500

    add base functional

---
+++ questions.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      {text: 'HyperSex Marker Language', correct: false},
+      {text: 'Hyper Markup Language', correct:  false},
+      {text: 'HyperVeryText Markup Language', correct: false},
+      {text: 'HyperText Markup Language', correct:  true}
+    ]
+  },
+  {
+    question: "What does CSS stand for?",
+    answers: [
+      {text: 'Cheats Style Sleep', correct: false},
+      {text: 'Clown Style Sheets', correct:  false},
+      {text: 'Cascading Style Sheets', correct: true},
+      {text: 'Cyber Style Sun', correct:  false}
+    ]
+  },
+  {
+    question: "When appeared JS?",
+    answers: [
+      {text: '1995', correct: true},
+      {text: '1895', correct:  false},
+      {text: '2001', correct: false},
+      {text: '1990', correct:  false}
+    ]
+  },
+  {
+    question: "What is DOM?",
+    answers: [
+      {text: 'Document Object Master', correct: false},
+      {text: 'Document Object Model', correct: true},
+      {text: 'Document Oriented Model', correct: false},
+      {text: 'DotsOracleMuseum', correct:  false}
+    ]
+  },
+]
---
+++ script.js
@@ -0,0 +1,117 @@
+const startBtn = document.querySelector('.start')
+const nextBtn = document.querySelector('.next-btn')
+const quiz = document.querySelector('.quiz-box')
+const question = document.querySelector('.question')
+const listQuiz = document.querySelector('.list-quiz')
+const counter = document.querySelector('.counter')
+
+counter.classList.add('hide')
+
+let counterCorrectAnswers = 1
+
+
+let questionsArray,
+  currentQuestionIndex;
+
+// Очищаем классы у элементов
+const clearStatusClass = (elem) => {
+  elem.classList.remove('correct')
+  elem.classList.remove('wrong')
+}
+
+// при переходе на другой вопрос удаляем старые ответы и чистим классы у body
+const resetState = () => {
+  nextBtn.style.display = 'none'
+
+  while (listQuiz.firstChild) {
+    listQuiz.removeChild(listQuiz.firstChild)
+  }
+}
+
+// добавляем классы "правильно" "не правильно" элементам при выборе
+const setStatusClass = (elem, status) => {
+
+  clearStatusClass(elem)
+
+  if (status) {
+    elem.classList.add('correct')
+  } else {
+    elem.classList.add('wrong')
+  }
+}
+
+// выбор ответа и добавление класса при нажатии
+const selectAnswer = (e) => {
+
+  const selectBtn = e.target
+
+  if (selectBtn.dataset.correct) {
+    counter.innerHTML = `
+    Your score: ${counterCorrectAnswers++}/${questionsArray.length}
+    <br>
+    ${counterCorrectAnswers > 3 ? "Test passed!" : "Test failed!"}
+    `
+  } else {
+    selectBtn.style.backgroundColor = 'red'
+  }
+
+  const correct = selectBtn.dataset.correct
+  setStatusClass(document.body, correct)
+
+  Array.from(listQuiz.children).forEach(btn => {
+    setStatusClass(btn, btn.dataset.correct)
+  })
+  if (questionsArray.length > currentQuestionIndex + 1) {
+    nextBtn.style.display = 'block'
+  } else {
+    setTimeout(() => {
+      counter.style.display = 'block'
+      quiz.classList.add('hide')
+    }, 1000)
+  }
+}
+
+
+
+// показываем вопрос и ответы
+const showQuestions = (arr) => {
+  resetState()
+  question.innerHTML = arr.question
+
+  arr.answers.forEach(answer => {
+
+    const li = document.createElement('li')
+    li.classList.add('list-quiz__item')
+    li.innerHTML = answer.text
+
+    if (answer.correct) {
+      li.dataset.correct = answer.correct
+    }
+
+    li.addEventListener('click', selectAnswer)
+    listQuiz.appendChild(li)
+
+  })
+}
+
+// выводим вопрос на страницу
+const setNextQuestions = function () {
+  showQuestions(questionsArray[currentQuestionIndex])
+}
+
+
+
+const startGame = function () {
+  this.style.display = 'none'
+  questionsArray = questions
+  currentQuestionIndex = 0
+  quiz.classList.remove('hide')
+  setNextQuestions()
+}
+
+
+startBtn.addEventListener('click', startGame)
+nextBtn.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestions()
+})
